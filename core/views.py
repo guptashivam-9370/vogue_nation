cebbe34ffcefac70b6972474a45eb682f006754e
guptashivam-9370 <shivamg9425@gmail.com>
2023-12-11 10:28:28 +0530

    popup to backend

--- core/views.py
+++ core/views.py
@@ -20,40 +20,40 @@ def register(request,competition_name):
         num_participants = len(names)
         team = Team.objects.create(
             team_name=team_name,
-            competition=competition,          
+            competition=competition, 
         )
         competitionss = Competition.objects.get(competition_name=competition_name)
         roles = Role.objects.filter(competitions=competitionss)
         competitions = Competition.objects.all()
-        role_counts = {}
-        for i in range(len(names)):
-            name = names[i]
-            email = emails[i]
-            phone_number = phone_numbers[i]
+        # role_counts = {}
+        # for i in range(len(names)):
+        #     name = names[i]
+        #     email = emails[i]
+        #     phone_number = phone_numbers[i]
 
-            # Validate name
-            if not name or not (email) or not phone_number or not team_name or  not phone_number.isdigit():
-                messages.error(request, f"Please enter All nessacary details properly")
+        #     # Validate name
+        #     if not name or not (email) or not phone_number or not team_name or  not phone_number.isdigit():
+        #         messages.error(request, f"Please enter All nessacary details properly")
 
-                return render(request, 'core/reg_vog.html', {
-                'competition' : competition_name,
-                'roles': roles,
-                'competitions': competitions,
-                })
-        for role in roles:
-            role_counts[role]=0
-        for i in range(num_participants):
-            role = get_object_or_404(Role, pk=int(role_ids[i]))
-            role_counts[role]+=1
-        for role, count in role_counts.items():
-            if count < role.min_member:
-                messages.error(request, f'The role {role} requires at least {role.min_member} participants.')
-                return render(request, 'core/reg_vog.html', {
-                'roles': roles,
-                'competitions': competitions,
-                })
+        #         return render(request, 'core/reg_vog.html', {
+        #         'competition' : competition_name,
+        #         'roles': roles,
+        #         'competitions': competitions,
+        #         })
+        # for role in roles:
+        #     role_counts[role]=0
+        # for i in range(num_participants):
+        #     role_counts[role]+=1
+        # for role, count in role_counts.items():
+        #     if count < role.min_member:
+        #         messages.error(request, f'The role {role} requires at least {role.min_member} participants.')
+        #         return render(request, 'core/reg_vog.html', {
+        #         'roles': roles,
+        #         'competitions': competitions,
+        #         })
         team.save()
         for i in range(num_participants):
+            role = get_object_or_404(Role, pk=int(role_ids[i]))
             memberdetails = Member_Detail.objects.create(
                 name=names[i],
                 email=emails[i],
